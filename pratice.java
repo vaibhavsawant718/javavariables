0f0cdb0c967eb69bead76226bea7d3f055164feb
vaibhav sawant <vaibhavsawant7011@gmail.com>
2025-01-11 18:44:16 +0530

    Practice file

---
+++ pratice.java
@@ -0,0 +1,14 @@
+class test 
+{
+	public static void main (String [] args)
+	{
+		int x=10;
+		int y=20;
+		int add;
+		int mul;
+		add=x+y;
+		mul=add*x;
+		System.out.println(add);
+		System.out.println(mul);
+	}
+}
